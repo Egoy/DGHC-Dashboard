48a6cd012b801167115e1bd2d6d4402ad31fad67
Egoy <54276210+Egoy@users.noreply.github.com>
2019-09-17 10:52:37 +0800

    Update search-withnav.php
    Updated with navigation displayed

--- search-withnav.php
+++ search-withnav.php
@@ -5,9 +5,7 @@ if (!isset($_SESSION['loggedin'])) {
 	header('Location: login.php');
 	exit();
 }
-
 include('includes/connections.php');
-
 if(isset($_POST['search']))
 {
     $fnameSearch = mysqli_real_escape_string($conn, $_POST['fnameSearch']);
@@ -19,7 +17,6 @@ else {
     $query = "SELECT * FROM `dghdbtbl` LIMIT 40";
     $search_result = filterTable($query);
 }
-
 function filterTable($query)
 {
     $connect = mysqli_connect("localhost", "root", "", "dghcdb");
@@ -37,36 +34,38 @@ function filterTable($query)
     <title>Document</title>
     <link rel="stylesheet" href="search.css">
     <link href="https://fonts.googleapis.com/css?family=Poppins:400,600&display=swap" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-    <script>
-    $('.Complex').click(function () {
-        $(this).toggleClass("collapsed").nextUntil('tr.Complex').toggle();
-    });
-    </script>
+
 </head>
 <body>
     <header>
-        <div class="heading">
-            <h1>DGCH Customer Dashboard</h1>
-            <form class="searchfield" action="search.php" method="post">
-                <h2>Enter Customer's Name or Address</h2>
+        <h1>DGCH Customer Dashboard</h1>
+            <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Add New Customer</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Logout</a></li>
+            </ul>
+        </nav>
+        <form class="searchfield" action="search.php" method="post">
                 <div class="container">
-                <input class="searchbox" type="text" name="fnameSearch" tabindex="1" autocomplete="off">
-                <!-- <input class="searchbox" type="text" name="adrSearch" placeholder="Customer's Name" tabindex="2" autocomplete="off"> -->
-                <input type="submit" name="search" value="SEARCH">
+                    <h2>Enter Customer's Name or Address</h2>
+                    <input class="searchbox" type="text" name="fnameSearch" tabindex="1" autocomplete="off">
+                    <!-- <input class="searchbox" type="text" name="adrSearch" placeholder="Customer's Name" tabindex="2" autocomplete="off"> -->
+                    <input type="submit" name="search" value="SEARCH">
                 </div>
-            </form>
-        </div>
+        </form>
     </header>
-    <nav>
-    <!-- <a href="#">Home</a>
-    <a href="#">Add New Customer</a>
-    <a href="#">Check Order Status</a>
-    <a href="#">Logout</a>         -->
-    </nav>
     <section>
-            <table id="table_detail">
-            <tr><th>ID</th><th>Customer Name</th><th>Address</th><th>Landmark</th><th>Tank Type</th><th>Send to SuperHub</th></tr>
+            <table id="tableMain">
+            <tr>
+                <th>ID</th>
+                <th>Customer Name</th>
+                <th>Address</th>
+                <th>Landmark</th>
+                <th>Tank Type</th>
+                <!-- <th>Send to SuperHub</th> -->
+            </tr>
             <?php
             include("includes/connections.php");
                 
@@ -82,29 +81,37 @@ function filterTable($query)
                         $db_lmark = $row["lmark"];
                         $db_ttype = $row["ttype"];
                         
-                        echo "<tr class='Complex'><td>$db_ID</td>";
-                        echo "<td>$db_fname $db_lname</td>";
-                        echo "<td>$db_adr</td>";
-                        echo "<td>$db_lmark</td>";
-                        echo "<td>$db_ttype</td>";
-                        echo "<td class='sendsms'><select>
-                        <option>----</option>
-                        <option value='BAY'>Bayan</option>
-                        <option value='BB'>Bambang</option>
-                        <option value='TIP'>Tipas</option>
-                        <option value='MLQ'>MLQ</option>
-                        <option value='SIG'>Signal</option>
-                      </select><button type='submit' value='Submit'>Send</button>
-                      </td>";
-                        echo "</tr>";
-                        echo "<tr class='hidden_row'>";
-                        echo "<td colspan=6><a href='#'>Edit Customer Info</a>
-                        <a href='#'>Delete Customer Data</a>
-                        </td>";
-                        echo "</tr>";
+                        echo    "<tr class='breakrow'>
+                                <td>$db_ID</td>
+                                <td>$db_fname $db_lname</td>
+                                <td>$db_adr</td>
+                                <td>$db_lmark</td>
+                                <td>$db_ttype</td>";
+                        // echo    "<td><select>
+                        //         <option>----</option>
+                        //         <option value='BAY'>Bayan</option>
+                        //         <option value='BB'>Bambang</option>
+                        //         <option value='TIP'>Tipas</option>
+                        //         <option value='MLQ'>MLQ</option>
+                        //         <option value='SIG'>Signal</option>
+                        //         </select><button type='submit' value='Submit'>Send</button>
+                        //         </td>";
+                        echo    "</tr>";
+                        echo    "<tr class='datarow' style='display:none'><td colspan='5'>
+                                <a href='#'>Edit Customer Info</a>
+                                <a href='#'>Delete Customer Data</a></td></tr></div>";
                 }
         ?>
         </table>
         </section>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script>
+        //collapse and expand sections
+
+//$('.breakrow').click(function(){
+$('#tableMain').on('click', 'tr.breakrow',function(){
+    $(this).nextUntil('tr.breakrow').slideToggle(100);
+});
+</script>
 </body>
 </html>
